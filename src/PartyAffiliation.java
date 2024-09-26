7d93898d651cf0ee242ee304c98e9f481a352e77
617998 <sinchuri.617998@swl.k12.oh.us>
2024-09-26 09:34:07 -0400

    f

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.Scanner;
+
+public class PartyAffiliation {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variable
+        String userPartyAffiliation;
+
+        //asks the user the party they are associated with
+        System.out.println("What is the party you are associated with? Please type in D for democrat, R for republican, I for independent and other.");
+        userPartyAffiliation = scan.nextLine();
+
+        if (userPartyAffiliation.equalsIgnoreCase("D")) {
+            System.out.println("You are a Democratic Donkey!");
+        } else if (userPartyAffiliation.equalsIgnoreCase("R")) {
+            System.out.println("You are a Republican Elephant!");
+        } else if (userPartyAffiliation.equalsIgnoreCase("I")) {
+            System.out.println("You are an Independent person.");
+        } else if (userPartyAffiliation.equalsIgnoreCase("other")) {
+            System.out.println("You are not associated with any of the above.");
+        } else {
+            System.out.println("You did not type a valid input or you misspelled.");
+            System.exit(0);
+        }
     }
 }
